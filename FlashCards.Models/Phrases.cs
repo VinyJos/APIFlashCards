5740c646058ac19dc7390cc9e9ff3f9cfa0123f6
Vinicius Jose <viniciuswk@gmail.com>
2023-01-10 21:10:05 -0300

    Criação da biblioteca e classe para a tabela

---
+++ FlashCards.Models/Phrases.cs
@@ -0,0 +1,9 @@
+﻿namespace FlashCards.Models
+{
+    public class Phrases
+    {
+        public int Id { get; set; }
+        public string TextEnglish { get; set; }
+        public string TextPortuguese { get; set; }
+    }
+}
